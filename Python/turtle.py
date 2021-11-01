efa885a87ba65f18150978a0d52b2b2ca035500a
vaibhavkausal1998 <44508632+vaibhavkausal1998@users.noreply.github.com>
2021-11-01 10:06:51 +0530

    Create turtle.py

---
+++ Python/turtle.py
@@ -0,0 +1,17 @@
+# import the turtle modules
+import turtle
+
+# Start a work Screen
+ws = turtle.Screen()
+
+# Define a Turtle Instance
+geekyTurtle = turtle.Turtle()
+
+# executing loop 6 times for 6 sides
+for i in range(6):
+
+	# Move forward by 90 units
+	geekyTurtle.forward(90)
+
+	# Turn left the turtle by 300 degrees
+	geekyTurtle.left(300)
